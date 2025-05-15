5b9aa4d2f7893b42d8fa70326aa19db9dbfab59b
HirakuTOIDA <hirakutoida@gmail.com>
2025-05-16 00:32:25 +0900

    Constant variable names are changed. Formatter is applied. Comments are partially translated to English.

--- ubx2CSV.py
+++ ubx2CSV.py
@@ -7,6 +7,7 @@ import tkinter as tk
 import tkinter.filedialog
 import ublox
 
+
 class Application(tk.Frame):
     """class for GUI."""
 
@@ -14,65 +15,100 @@ class Application(tk.Frame):
         super().__init__(master)
         self.master = master
         self.pack()
-        self.master.title('ubx2CSV')
+        self.master.title("ubx2CSV")
 
-        _pad = [5,5]
+        _pad = [5, 5]
 
-        self.version_list = ["6", "7", "8", "9"]
-        self.version_list_len = len(self.version_list)
+        UBLOX_GENERATIONS = [6, 7, 8, 9]
+        UBLOX_GENERATIONS_LEN = len(UBLOX_GENERATIONS)
 
-        # ラベル
-        self.lb0 = tk.Label(self, text=u'u-blox generation: ')
-        self.lb0.grid(row=0, column=0, columnspan=self.version_list_len,
-                      padx=_pad[0], pady=_pad[1], sticky=tk.W)
+        # label
+        self.lb0 = tk.Label(self, text="u-blox generation: ")
+        self.lb0.grid(
+            row=0,
+            column=0,
+            columnspan=UBLOX_GENERATIONS_LEN,
+            padx=_pad[0],
+            pady=_pad[1],
+            sticky=tk.W,
+        )
 
-        # ラジオボタン
+        # radio button
         self.var = tk.IntVar()
         self.var.set(9)
-        self.rbs = [tk.Radiobutton(self, value=6+i, variable=self.var, text=self.version_list[i]) for i in range(self.version_list_len)]
-        [self.rbs[i].grid(row=1, column=i, columnspan=1, padx=_pad[0], pady=_pad[1], sticky=tk.W) for i in range(self.version_list_len)]
+        self.rbs = [
+            tk.Radiobutton(
+                self, value=6 + i, variable=self.var, text=UBLOX_GENERATIONS[i]
+            )
+            for i in range(UBLOX_GENERATIONS_LEN)
+        ]
+        [
+            self.rbs[i].grid(
+                row=1, column=i, columnspan=1, padx=_pad[0], pady=_pad[1], sticky=tk.W
+            )
+            for i in range(UBLOX_GENERATIONS_LEN)
+        ]
 
-        # ボタン
-        self.bt = tk.Button(self, text=u'Open & Convert',
-                            command = self.fileopen)
-        self.bt.grid(row=self.version_list_len+1, column=0,
-                     columnspan=self.version_list_len,
-                     padx=_pad[0], pady=_pad[1], sticky=tk.W+tk.E)
+        # button
+        self.bt = tk.Button(self, text="Open & Convert", command=self.fileopen)
+        self.bt.grid(
+            row=UBLOX_GENERATIONS_LEN + 1,
+            column=0,
+            columnspan=UBLOX_GENERATIONS_LEN,
+            padx=_pad[0],
+            pady=_pad[1],
+            sticky=tk.W + tk.E,
+        )
 
-        # ラベル
+        # label
         self.filename_str = tk.StringVar()
-        self.filename_str.set(u"File name: ")
+        self.filename_str.set("File name: ")
         self.lbfn = tk.Label(self, textvariable=self.filename_str)
-        self.lbfn.grid(row=self.version_list_len+2, column=0,
-                       columnspan=int(self.version_list_len/2),
-                       padx=_pad[0], pady=_pad[1], sticky=tk.W)
+        self.lbfn.grid(
+            row=UBLOX_GENERATIONS_LEN + 2,
+            column=0,
+            columnspan=int(UBLOX_GENERATIONS_LEN / 2),
+            padx=_pad[0],
+            pady=_pad[1],
+            sticky=tk.W,
+        )
 
-        # ラベル
+        # label
         self.filesize_str = tk.StringVar()
-        self.filesize_str.set(u"File size: ")
+        self.filesize_str.set("File size: ")
         self.lbfs = tk.Label(self, textvariable=self.filesize_str)
-        self.lbfs.grid(row=self.version_list_len+3, column=0,
-                       columnspan=int(self.version_list_len/2),
-                       padx=_pad[0], pady=_pad[1], sticky=tk.W)
+        self.lbfs.grid(
+            row=UBLOX_GENERATIONS_LEN + 3,
+            column=0,
+            columnspan=int(UBLOX_GENERATIONS_LEN / 2),
+            padx=_pad[0],
+            pady=_pad[1],
+            sticky=tk.W,
+        )
 
-        # ラベル
+        # label
         self.status_str = tk.StringVar()
-        self.status_str.set(u"Select file.")
+        self.status_str.set("Select file.")
         self.lbst = tk.Label(self, textvariable=self.status_str)
-        self.lbst.grid(row=self.version_list_len+4, column=0,
-                       columnspan=int(self.version_list_len/2),
-                       padx=_pad[0], pady=_pad[1], sticky=tk.W)
+        self.lbst.grid(
+            row=UBLOX_GENERATIONS_LEN + 4,
+            column=0,
+            columnspan=int(UBLOX_GENERATIONS_LEN / 2),
+            padx=_pad[0],
+            pady=_pad[1],
+            sticky=tk.W,
+        )
 
     def fileopen(self):
         """Open button."""
-        fTyp = [("ubx file","*.ubx")]
-        filename = tk.filedialog.askopenfilename(filetypes = fTyp)
+        fTyp = [("ubx file", "*.ubx")]
+        filename = tk.filedialog.askopenfilename(filetypes=fTyp)
         dirname = os.path.dirname(filename)
         if filename:
             os.chdir(dirname)
         if len(filename) > 0:
-            self.bt.configure(state = tk.DISABLED)
-            self.status_str.set(u"File selected.")
+            self.bt.configure(state=tk.DISABLED)
+            self.status_str.set("File selected.")
             th = threading.Thread(target=self.convert, args=(filename,))
             th.start()
 
@@ -88,17 +124,17 @@ class Application(tk.Frame):
 
         # 各UBXメッセージに対してインスタンス生成
         for msg_key, msg_def in ubx_messages.items():
-
-        with open(filename, 'rb') as fobj:
-            with open("ubx2CSV.log", 'w') as fobjlog:
-                self.filename_str.set(u"File name: " + filename)
             try:
                 ubx_instances[msg_key] = ublox.ublox(msg_def)
             except:
                 print("Error in ublox class generation")
+
+        with open(filename, "rb") as fobj:
+            with open("ubx2CSV.log", "w") as fobjlog:
+                self.filename_str.set("File name: " + filename)
                 filesize = os.path.getsize(filename)
-                self.filesize_str.set(u"File size: {0:,} byte".format(filesize))
-                self.status_str.set(u"File opened.")
+                self.filesize_str.set("File size: {0:,} byte".format(filesize))
+                self.status_str.set("File opened.")
                 name, _ = os.path.splitext(filename)
 
                 message_count = 0
@@ -108,34 +144,34 @@ class Application(tk.Frame):
                 checksum_error_count = 0
                 pb_previous = 0
 
-                self.status_str.set(u"Reading file.")
+                self.status_str.set("Reading file.")
                 while True:
-                    if message_count == 0: #ヘッダのチェック 1
-                        h_data = fobj.read(1) # 1バイトずつ
+                    if message_count == 0:  # ヘッダのチェック 1
+                        h_data = fobj.read(1)  # 1バイトずつ
                         read_count += len(h_data)
                         if len(h_data) < 1:
                             break
-                        if h_data == ublox.ubx_sync_char[0].to_bytes(1, 'big'):
+                        if h_data == ublox.ubx_sync_char[0].to_bytes(1, "big"):
                             message_count += 1
-                    elif message_count == 1: # ヘッダのチェック 2
-                        h_data = fobj.read(1) # 1バイトずつ
+                    elif message_count == 1:  # ヘッダのチェック 2
+                        h_data = fobj.read(1)  # 1バイトずつ
                         read_count += len(h_data)
                         if len(h_data) < 1:
                             break
-                        if h_data == ublox.ubx_sync_char[1].to_bytes(1, 'big'):
+                        if h_data == ublox.ubx_sync_char[1].to_bytes(1, "big"):
                             ubx_count += 1
                             message_count += 1
                         else:
                             message_count = 0
-                    elif message_count == 2: # ヘッダが見つかった場合
+                    elif message_count == 2:  # ヘッダが見つかった場合
                         # class, id, lengthの読み込み
                         h_data = fobj.read(4)
                         read_count += len(h_data)
                         if len(h_data) < 4:
                             break
                         dat = bytearray(h_data)
-                        ubx_class_id = int.from_bytes(h_data[:2], 'big')
-                        ubx_length = int.from_bytes(h_data[2:], 'little')
+                        ubx_class_id = int.from_bytes(h_data[:2], "big")
+                        ubx_length = int.from_bytes(h_data[2:], "little")
                         # length分だけメッセージの読み込み
                         h_data = fobj.read(ubx_length)
                         read_count += len(h_data)
@@ -150,37 +186,49 @@ class Application(tk.Frame):
                             break
                         pb_current = int(read_count / filesize * 100)
                         if pb_previous < pb_current:
-                            self.status_str.set(u"Reading file. {}% done.".format(pb_current))
+                            self.status_str.set(
+                                "Reading file. {}% done.".format(pb_current)
+                            )
                             pb_previous = pb_current
-                        checksum_data = int.from_bytes(h_data, 'little')
+                        checksum_data = int.from_bytes(h_data, "little")
                         if checksum_data != ch:
-                            fobjlog.write(f"Checksum error: ubx count={ubx_count:,}, class/id=0x{ubx_class_id:04X}, length={ubx_length:,}, checksum data=0x{checksum_data:04X}, checksum calculated=0x{ch:04X}\n")
+                            fobjlog.write(
+                                f"Checksum error: ubx count={ubx_count:,}, class/id=0x{ubx_class_id:04X}, length={ubx_length:,}, checksum data=0x{checksum_data:04X}, checksum calculated=0x{ch:04X}\n"
+                            )
                             # @todo 戻る?
                             checksum_error_count += 1
                         else:
-                            if ubx_class_id in ubx_messages: # class, idが見つかった場合
+                            if (
+                                ubx_class_id in ubx_messages
+                            ):  # class, idが見つかった場合
                                 if len(dat[4:]) == 0:
-                                    fobjlog.write(f"No data contained: ubx count={ubx_count:,}, class/id=0x{ubx_class_id:04X}, length={ubx_length:,}\n")
+                                    fobjlog.write(
+                                        f"No data contained: ubx count={ubx_count:,}, class/id=0x{ubx_class_id:04X}, length={ubx_length:,}\n"
+                                    )
                                 else:
-                            else: # class, idが見つからなかった場合
-                                fobjlog.write(f"Message class/id not found: ubx count={ubx_count:,}, class/id=0x{ubx_class_id:04X}, length={ubx_length:,}\n")
                                     try:
                                         ubx_instances[ubx_class_id].append(dat[4:])
                                         convert_count += 1
                                     except:
                                         print(f"Error in appending ublox message.")
+                            else:  # class, idが見つからなかった場合
+                                fobjlog.write(
+                                    f"Message class/id not found: ubx count={ubx_count:,}, class/id=0x{ubx_class_id:04X}, length={ubx_length:,}\n"
+                                )
                         message_count = 0
 
-                self.status_str.set(u"Writing csv files.")
+                self.status_str.set("Writing csv files.")
                 print("Saved UBX Messages")
                 for ubx_class_id in ubx_messages:
                     try:
-                        ubx_instances[ubx_class_id].save_csv(f"{ubx_messages[ubx_class_id][0]}.csv")
+                        ubx_instances[ubx_class_id].save_csv(
+                            f"{ubx_messages[ubx_class_id][0]}.csv"
+                        )
                         print(f"0x{ubx_class_id:04X}: Done")
                     except:
                         print(f"0x{ubx_class_id:04X}: Error")
 
-                self.status_str.set(u"Writing log file.")
+                self.status_str.set("Writing log file.")
                 fobjlog.write("\nSummary of the conversion\n")
                 fobjlog.write(f"Source: {filename}\n")
                 fobjlog.write(f"Filesize:  {filesize:,} bytes\n")
@@ -189,10 +237,11 @@ class Application(tk.Frame):
                 fobjlog.write(f"ubx messages converted: {convert_count:,}\n")
                 fobjlog.write(f"checksum error count: {checksum_error_count:,}\n")
 
-                self.status_str.set(u"Done.")
-                self.bt.configure(state = tk.NORMAL)
+                self.status_str.set("Done.")
+                self.bt.configure(state=tk.NORMAL)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     root = tk.Tk()
     app = Application(master=root)
     app.mainloop()
